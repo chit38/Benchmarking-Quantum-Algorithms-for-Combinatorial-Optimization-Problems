23c56ce44db8e9d5ce2199c22df50bc384adfb03
Chitresh Chaudhari <47925131+chit38@users.noreply.github.com>
2020-09-07 15:13:49 +0530

    Delete using_numpyeigensolver.py

--- using_numpyeigensolver.py
+++
@@ -1,8 +0,0 @@
-def using_numpyeigensolver(n, adj_matrix):
-    qubitop, offset = max_cut.get_operator(adj_matrix)
-    qudratic = IsingToQuadraticProgram()
-    qudratic_problem = qudratic.encode(qubitop, offset)
-    solver = MinimumEigenOptimizer(NumPyMinimumEigensolver())
-    result = solver.solve(qudratic_problem)
-    best_cost = float(str(result).split('-')[1])
-    return best_cost
